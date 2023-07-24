0b7973e8d5a09e837627272d62d17554431ffed8
Pierre Michel <pmiche04@gmail.com>
2023-07-24 08:22:20 -0600

    Testing the shouldEnableMaxHeight props
    Signed-off-by: Pierre Michel <pmiche04@gmail.com>

--- src/pages/settings/Security/TwoFactorAuth/VerifyPage.js
+++ src/pages/settings/Security/TwoFactorAuth/VerifyPage.js
@@ -93,7 +93,7 @@ function VerifyPage(props) {
     }
 
     return (
-        <ScreenWrapper shouldShowOfflineIndicator={false}>
+        <ScreenWrapper shouldShowOfflineIndicator={false}  shouldEnableMaxHeight>
             <HeaderWithBackButton
                 title={props.translate('twoFactorAuth.headerTitle')}
                 stepCounter={{
